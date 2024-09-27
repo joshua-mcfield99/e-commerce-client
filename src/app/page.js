9b88197b5e480497aff9396b6fc80380810f2ac3
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2024-09-27 16:27:49 +0100

    Landing page first run

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,49 @@
 import Link from 'next/link';
 import styles from "./page.module.css";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <div>
-            <section className="hero">
-              <h1>Welcome to Crooked Clothing</h1>
-              <p>Find your style with our latest collections.</p>
-            </section>
+    
+    const items = [
+        { id: 1, src: '/Hoodie_Black.png', alt: 'This is our Hoodie in black' },
+        { id: 2, src: '/T-shirt_black.png', alt: 'This is our T-shirt in black' },
+        { id: 3, src: '/Jumper_Black.png', alt: 'This is our Jumper in black' },
+    ];
+    
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <div>
+                    <section className={`${styles.hero}`}>
+                    <h1>This is Crooked Clothing.</h1>
+                    <p>Find your style with our latest collections.</p>
+                    </section>
 
-            <section className="popular-items">
-              <h2>Popular Items</h2>
-              {/* Map through popular items and show them */}
-              {/* On click, you can open a modal with product details */}
-            </section>
-
-            <Link href="/mens">Shop Men&#39;s Clothing</Link>
-            <Link href="/womens">Shop Women&#39;s Clothing</Link>
+                    <section className={styles.popular_items}>
+                        <h2>Popular Items</h2>
+                        <div className={styles.track}>
+                            <div className={styles.track_inner}>
+                                {/* Map through items twice to create a loop */}
+                                {items.concat(items).map((item, index) => (
+                                    <div className={styles.track_item} key={index}>
+                                        <Image
+                                            alt={item.alt}
+                                            src={item.src}
+                                            width={250}
+                                            height={250}
+                                            style={{ objectFit: 'contain', objectPosition: 'center center', margin: 'auto'}}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    </section>
+                    <section className={`${styles.links}`}>
+                        <Link href="/mens">Shop Men&#39;s</Link>
+                        <Link href="/womens">Shop Women&#39;s</Link>
+                    </section>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
